ac7c54daf0776e3733fec2814d9136caed642de5
radouane99 <radouane.asri99@gmail.com>
2026-05-10 23:59:23 +0100

    chore: add real Moroccan and thematic books to seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -49,7 +49,90 @@ class DatabaseSeeder extends Seeder
             'photo'=>'Inkonnu',
             'password'=> Hash::make('ahmed')
         ]);
-        Theme::factory()->has(Livre::factory(4))->count(4)->create();
+        // Création des Thèmes
+        $themeLitterature = Theme::create([
+            'codeTh' => 'TH01',
+            'intitule' => 'Littérature Marocaine'
+        ]);
+
+        $themeInfo = Theme::create([
+            'codeTh' => 'TH02',
+            'intitule' => 'Informatique & Technologies'
+        ]);
+
+        $themeScience = Theme::create([
+            'codeTh' => 'TH03',
+            'intitule' => 'Sciences'
+        ]);
+
+        $themeArt = Theme::create([
+            'codeTh' => 'TH04',
+            'intitule' => 'Arts & Culture'
+        ]);
+
+        // Création des Livres Marocains et autres
+        Livre::create([
+            'codeL' => 'L001',
+            'titre' => 'La Boîte à Merveilles',
+            'auteur' => 'Ahmed Sefrioui',
+            'nbExemplaire' => 10,
+            'theme_id' => $themeLitterature->id,
+            'couverture' => 'photos/boite_merveilles.jpg'
+        ]);
+
+        Livre::create([
+            'codeL' => 'L002',
+            'titre' => 'Le Pain Nu',
+            'auteur' => 'Mohamed Choukri',
+            'nbExemplaire' => 5,
+            'theme_id' => $themeLitterature->id,
+            'couverture' => 'photos/pain_nu.jpg'
+        ]);
+
+        Livre::create([
+            'codeL' => 'L003',
+            'titre' => 'L\'Enfant de Sable',
+            'auteur' => 'Tahar Ben Jelloun',
+            'nbExemplaire' => 7,
+            'theme_id' => $themeLitterature->id,
+            'couverture' => 'photos/enfant_sable.jpg'
+        ]);
+
+        Livre::create([
+            'codeL' => 'L004',
+            'titre' => 'Maîtriser Laravel 11',
+            'auteur' => 'Taylor Otwell',
+            'nbExemplaire' => 12,
+            'theme_id' => $themeInfo->id,
+            'couverture' => 'photos/laravel.jpg'
+        ]);
+
+        Livre::create([
+            'codeL' => 'L005',
+            'titre' => 'Intelligence Artificielle',
+            'auteur' => 'Yann LeCun',
+            'nbExemplaire' => 8,
+            'theme_id' => $themeInfo->id,
+            'couverture' => 'photos/ia.jpg'
+        ]);
+
+        Livre::create([
+            'codeL' => 'L006',
+            'titre' => 'L\'Univers Élégant',
+            'auteur' => 'Brian Greene',
+            'nbExemplaire' => 4,
+            'theme_id' => $themeScience->id,
+            'couverture' => 'photos/univers.jpg'
+        ]);
+
+        Livre::create([
+            'codeL' => 'L007',
+            'titre' => 'Histoire de l\'Art au Maroc',
+            'auteur' => 'Farid Belkahia',
+            'nbExemplaire' => 3,
+            'theme_id' => $themeArt->id,
+            'couverture' => 'photos/art_maroc.jpg'
+        ]);
 
         // Theme::factory(4)->create();
     }
